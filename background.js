a46da2123cba6dc25599921a5a5a712b4beb5db8
Shu Lin <linshu123@gmail.com>
2014-08-15 13:21:39 -0700

    fixed undefined obj problem

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
+chrome.runtime.onConnect.addListener(function(port) {
+  var tab = port.sender.tab;
+
+  // This will get called by the content script we execute in
+  // the tab as a result of the user pressing the browser action.
+  port.onMessage.addListener(function(info) {
+    var max_length = 1024;
+    if (info.selection.length > max_length)
+      info.selection = info.selection.substring(0, max_length);
+    chrome.tabs.executeScript(tab.id, {
+        "file": "contentScript.js"
+    });
+  });
+});
+
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
+    console.log("HWHHW");
     chrome.tabs.executeScript(tab.id, {
         "file": "contentScript.js"
     });
 });
+
